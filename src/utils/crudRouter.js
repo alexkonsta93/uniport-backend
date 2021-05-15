d3e4e377c9ea83154ba66b76b3d155ba85d004da
Alexandros Konstantopoulos <alexkonsta93@gmail.com>
2021-05-14 23:34:30 -0400

    Created crudRouter utility

---
+++ src/utils/crudRouter.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import crudControllers from './crud';
+
+var router = express.Router();
+
+// /api/exchange
+router
+  .route('/')
+  .get(crudControllers.getOne)
+  .post(crudControllers.createOne)
+  .delete(crudControllers.deleteOne);
+
+// /api/exchange/all
+router
+  .route('/all')
+  .get(crudControllers.getAll)
+  .delete(crudControllers.deleteAll);
+
+export default router;
