f1e84c329fa913fdd80dab49d264eed68cbc8bd1
Alexandros Konstantopoulos <alexkonsta93@gmail.com>
2021-05-14 21:55:35 -0400

    Work on user.model.js and user.router.js

--- src/resources/user/user.model.js
+++ src/resources/user/user.model.js
@@ -2,54 +2,31 @@ import mongoose from 'mongoose';
 var { Schema } = mongoose;
 
 var userSchema = new Schema({
-		firstName: {
-				type: String,
-				required: true
-		},
-		lastName: {
-				type: String,
-				required: true
-		},
-		orders: {
-				type: [mongoose.Schema.Types.ObjectId],
-				required: true,
-				ref: 'orders'
-		},
-		trades: {
-				type: [mongoose.Schema.Types.ObjectId],
-				required: true,
-				ref: 'trades'
-		},
-		exchanges: {
-				type: [new Schema({
-						name: String,
-						apiKey: Number
-				})],
-				required: true
-		}
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  orders: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: 'orders'
+  },
+  trades: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: 'trades'
+  },
+  exchanges: [{
+    exchangeId: mongoose.Schema.Types.ObjectId,
+    numTrades: Number,
+    lastImport: Date, 
+    numLastImport: Number
+  }]
 });
 
-userSchema.methods.updateAuto = function(exchanges) {
-		[{ exchangeName, apiKey }] = exchanges;
-
-		this.exchanges.forEach(({ exchangeName, apiKey }) => {
-				var client = clients["name"];
-
-				client.connect(apiKey)
-						.then(client => {
-								var data = await client.downloadUpdate();
-								await this.update(name, data);
-						})
-						.catch(error => {
-								throw new Error(error);
-						})
-		});
-}
-
-userSchema.methods.updateManual = function(data) {
-		await this.update(data);
-}
-
-userSchema.
-
 var User = mongoose.model('User', userSchema);
+export default User;
